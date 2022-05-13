3e23d2d0a7fd53c7b69a5c7b5fde766483090aa5
Smyja <akposlive59@gmail.com>
2022-05-13 05:24:58 +0100

    update

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Stories, Comment
 # Register your models here.
-admin.site.register(Stories)
+class StoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('synced',)
+admin.site.register(Stories, StoryAdmin)
 admin.site.register(Comment)
--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django_mysql.models import ListCharField
 
 # Create your models here.
@@ -7,21 +8,23 @@ class Stories(models.Model):
     story_id = models.IntegerField("Story ID", null=True)
     title = models.TextField("Title", null=True)
     story_type = models.CharField("Type of item", max_length=15, null=True)
-    synced = models.DateTimeField(auto_now_add=True, null=True)
+    synced = models.DateTimeField(auto_now_add=True)
     author = models.CharField("Author", max_length=50, null=True)
     kids= ListCharField(base_field=models.IntegerField(),max_length=(30*40),null=True)
     slug = models.SlugField(max_length=2000, null=True)
-    time = models.DateTimeField("Date created", null=True)
+    time = models.CharField("Date created", max_length=1000, null=True)
     text = models.TextField("The comment, story or poll text.", null=True)
     dead = models.BooleanField(default=False)
     url = models.URLField("URL", max_length=1000, null=True)
     score = models.IntegerField("Score", null=True)
-    descendants = models.IntegerField("Descendants", null=True)
-    
- 
+    descendants = models.IntegerField("Descendants",blank=True, null=True)
 
-    class Meta:
-        ordering = ('-synced',)
+
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Stories, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.title
@@ -30,7 +33,7 @@ class Comment(models.Model):
     comment_id = models.IntegerField("Story ID", null=True, unique=True)
     story = models.ForeignKey(Stories, on_delete=models.CASCADE, related_name="comments", null=True)
     author = models.CharField("Author", max_length=50, null=True)
-    time = models.DateTimeField("Date created", null=True)
+    time = models.CharField("Date created",max_length=1000,  null=True)
     text = models.TextField("Text", null=True)
     dead = models.BooleanField(default=False)
     comment_url = models.URLField("URL", max_length=1000, null=True)
@@ -38,4 +41,4 @@ class Comment(models.Model):
     
 
     def __str__(self):
-        return self.story.title
+        return self.title
